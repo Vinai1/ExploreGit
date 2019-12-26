2e035b41f9cbe70036c5d2acd7c51ceb6cb011ed
Vinai1 <vinaitp58@gmail.com>
2019-12-26 22:35:30 +0530

    Learning Git with eclipse by adding specific files

---
+++ LearnMaven/src/main/java/mavenExecutor/MavenExecution.java
@@ -0,0 +1,10 @@
+package mavenExecutor;
+
+public class MavenExecution {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Testing");
+	}
+
+}
---
+++ LearnMaven/src/test/java/testExecutor/TestExecution.java
@@ -0,0 +1,10 @@
+package testExecutor;
+
+public class TestExecution {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Testing Phase");
+	}
+
+}
